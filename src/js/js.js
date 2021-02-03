5ffab81c45b590c931847c0e15ba752947ef3185
Stefan Korder <xtybevip@googlemail.com>
2021-02-03 14:50:39 +0100

    JS added

---
+++ src/js/js.js
@@ -0,0 +1,63 @@
+const button1 = document.querySelector('#button1')
+const answer1 = document.querySelector('#answer1')
+
+button1.addEventListener('click',
+    () => {
+        answer1.classList.toggle('hidden')
+})
+
+const button2 = document.querySelector('#button2')
+const answer2 = document.querySelector('#answer2')
+
+button2.addEventListener('click',
+    () => {
+        answer2.classList.toggle('hidden')
+})
+
+const button3 = document.querySelector('#button3')
+const answer3 = document.querySelector('#answer3')
+
+button3.addEventListener('click',
+    () => {
+        answer3.classList.toggle('hidden')
+})
+
+const button4 = document.querySelector('#button4')
+const answer4 = document.querySelector('#answer4')
+
+button4.addEventListener('click',
+    () => {
+        answer4.classList.toggle('hidden')
+})
+
+const button5 = document.querySelector('#button5')
+const answer5 = document.querySelector('#answer5')
+
+button5.addEventListener('click',
+    () => {
+        answer5.classList.toggle('hidden')
+})
+
+const button6 = document.querySelector('#button6')
+const answer6 = document.querySelector('#answer6')
+
+button6.addEventListener('click',
+    () => {
+        answer6.classList.toggle('hidden')
+})
+
+const button7 = document.querySelector('#button7')
+const answer7 = document.querySelector('#answer7')
+
+button7.addEventListener('click',
+    () => {
+        answer7.classList.toggle('hidden')
+})
+
+const button8 = document.querySelector('#button8')
+const answer8 = document.querySelector('#answer8')
+
+button8.addEventListener('click',
+    () => {
+        answer8.classList.toggle('hidden')
+})
